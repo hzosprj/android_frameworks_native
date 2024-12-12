db023657552e6c12f1fd352cd3ec3e38ce3fa23e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-12-12 08:48:56 +0800

    SF: Defer setDesiredActiveMode when display is off
    This is required on some devices with video mode panel, since the
    scheduler sets refresh rate to 120 Hz _after_ turning screen off at
    60 Hz (VRR idle) but the HWC ignores this and when the display is
    turned back on SF thinks we are back at 120 Hz while HWC is still
    rendering at 60 Hz. Defer setting refresh rate until the screen is
    turned back on, to avoid this confusion.
    
    To enable, set: debug.sf.defer_refresh_rate_when_off=1
    
    Change-Id: I3e531e601e9cb015b3066ae98d4d9c5436b46619
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -519,6 +519,9 @@ SurfaceFlinger::SurfaceFlinger(Factory& factory) : SurfaceFlinger(factory, SkipI
     property_get("debug.sf.dim_in_gamma_in_enhanced_screenshots", value, 0);
     mDimInGammaSpaceForEnhancedScreenshots = atoi(value);
 
+    property_get("debug.sf.defer_refresh_rate_when_off", value, "0");
+    mDeferRefreshRateWhenOff = atoi(value);
+
     mIgnoreHwcPhysicalDisplayOrientation =
             base::GetBoolProperty("debug.sf.ignore_hwc_physical_display_orientation"s, false);
 
@@ -1386,8 +1389,15 @@ status_t SurfaceFlinger::getDisplayStats(const sp<IBinder>& displayToken,
 void SurfaceFlinger::setDesiredMode(display::DisplayModeRequest&& desiredMode) {
     const auto mode = desiredMode.mode;
     const auto displayId = mode.modePtr->getPhysicalDisplayId();
+    const auto desiredDisplay = getDisplayDeviceLocked(displayId);
 
     SFTRACE_NAME(ftl::Concat(__func__, ' ', displayId.value).c_str());
+
+    if (mDeferRefreshRateWhenOff && desiredDisplay && desiredDisplay->getPowerMode() == hal::PowerMode::OFF) {
+        ALOGI("%s: deferring because display is powered off", __func__);
+        mLastActiveMode = mode;
+        return;
+    }
 
     const bool emitEvent = desiredMode.emitEvent;
 
@@ -5756,6 +5766,11 @@ void SurfaceFlinger::setPhysicalDisplayPowerMode(const sp<DisplayDevice>& displa
         }
 
         getHwComposer().setPowerMode(displayId, mode);
+        if (mLastActiveMode) {
+            ALOGI("Deferred active mode change pending, applying now");
+            setDesiredMode({mLastActiveMode.value()});
+            mLastActiveMode = std::nullopt;
+        }
         if (mode != hal::PowerMode::DOZE_SUSPEND) {
             const bool enable =
                     mScheduler->getVsyncSchedule(displayId)->getPendingHardwareVsyncState();
